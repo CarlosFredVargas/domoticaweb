1134ae50da33bc4859ad8e4fd9d7187af99921ef
CarlosFredVargas <i2422863@continental.edu.pe>
2024-10-30 00:07:06 -0500

    creando los primeros pasos de la pagina

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("Gracias por contactarnos. Nos pondremos en contacto pronto.");
+    // Aquí podrías agregar código para enviar el formulario a un servidor
+});
